2a17b7579c160c2b51729d3e8e312c998097845f
Karol <karolostapinski9@gmail.com>
2026-03-20 14:52:32 +0100

    Finished

--- LAB3/cyclic_table.py
+++ LAB3/cyclic_table.py
@@ -17,7 +17,7 @@ class cyclic_table:
         if self.is_empty():
             return None
         value = self.peek()
-        self.read_idx = 0
+        self.tab[self.read_idx] = None
         self.read_idx = (self.read_idx + 1) % self.size
         return value
 
@@ -36,10 +36,20 @@ class cyclic_table:
 
     def __str__(self):
         string = '['
-        for i in range(self.size):
-            idx = (self.read_idx + i) % self.size
+        idx = (self.read_idx) % self.size
+        while idx != self.write_idx:
             string += f'{self.tab[idx]}'
-            if idx < self.size:
+            if (idx + 1) % self.size != self.write_idx:
+                string += ','
+            idx = (idx + 1) % self.size
+        string += ']'
+        return string
+    
+    def print_(self):
+        string = '['
+        for i in range(self.size):
+            string += str(self.tab[i])
+            if i < self.size - 1:
                 string += ','
         string += ']'
         return string
@@ -54,24 +64,15 @@ def main():
     table.enqueue(3)
     table.enqueue(4)
     print(table.dequeue())
-    print()
     print(table)
     table.enqueue(5)
     table.enqueue(6)
     table.enqueue(7)
     table.enqueue(8)
-    print(table)
-    for _ in range(table.size):
+    print(table.print_())
+    while table.read_idx != table.write_idx:
         print(table.dequeue())
     print(table)
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-    
-
