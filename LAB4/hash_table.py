e7dcc22d0a691a623ac5a7e0bfd6f87cda43b2d2
Karol <karolostapinski9@gmail.com>
2026-03-27 17:58:25 +0100

    LAB4 Finished obligatory task

--- LAB4/hash_table.py
+++ LAB4/hash_table.py
@@ -35,7 +35,9 @@ class static_tab:
         base_idx = self.hash(key)
         if self.tab[base_idx] is not None and self.tab[base_idx].key == key:
             return self.tab[base_idx].val
-        idx = self.resolve_collision(base_idx)
+        idx = self.resolve_collision(base_idx, key)
+        if idx is None:
+            return None
         if self.tab[idx] == None:
             return None
         else:
@@ -49,7 +51,7 @@ class static_tab:
         if self.tab[idx].key == key:
             self.tab[idx].val = val
             return
-        idx = self.resolve_collision(idx)
+        idx = self.resolve_collision(idx, key)
         if idx is None:
             return None, "Lack of space"
         if self.tab[idx] == None:
@@ -63,11 +65,61 @@ class static_tab:
         base_idx = self.hash(key)
         if self.tab[base_idx] is not None and self.tab[base_idx].key == key:
             self.tab[base_idx] = None
-        idx = self.resolve_collision(base_idx)
-        if self.tab[idx] != None:
+        idx = self.resolve_collision(base_idx, key)
+        if idx != None and self.tab[idx] != None and self.tab[idx].key == key:
             self.tab[idx] = None
         return
 
     def __str__(self):
-        print("{")
-        # for i in range(self.size):
+        str = '{'
+        for i in range(self.size):
+            if self.tab[i] is not None:
+                str += f'{self.tab[i].key}:{self.tab[i].val}'
+                if i < self.size - 1:
+                    str += ', '
+            else:
+                str += 'None, '
+        str += '}'
+        return str
+
+def main():
+    def Test_1(c1 = 0, c2 = 1):
+        numbers = [1,2,3,4,5,18,31,8,9,10,11,12,13,14,15]
+        values = [chr(i) for i in range(65, 65 + 15)]
+        Table_1 = static_tab(c1 = c1, c2 = c2, size = 13)
+        for num, val in zip(numbers, values):
+            Table_1.insert(val, num)
+        print(Table_1)
+        print(Table_1.search(5))
+        print(Table_1.search(14))
+        Table_1.insert('Z', 5)
+        print(Table_1.search(5))
+        Table_1.remove(5)
+        print(Table_1)
+        print(Table_1.search(31))
+        return Table_1
+    
+    Table = Test_1()
+    print("___________________________________________________________________________________________________________")
+    Table.insert('W', 'test')
+    print(Table)
+    print("___________________________________________________________________________________________________________")
+
+    def Test_2(c1 = 1, c2 = 0):
+        Table_2 = static_tab(c1 = c1, c2 = c2, size = 13)
+        numbers = [13 * i for i in range(1, 16)]
+        values = [chr(i) for i in range(65, 65 + 15)]
+        for num, val in zip(numbers, values):
+            Table_2.insert(val, num)
+        return print(Table_2)
+    
+    Test_2()
+    print("___________________________________________________________________________________________________________")
+    Test_2(0, 1)
+    print("___________________________________________________________________________________________________________")
+    Test_1(0, 1)
+
+
+if __name__ == "__main__":
+    main()
+    
